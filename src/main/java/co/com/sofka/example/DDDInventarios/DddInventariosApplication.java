8354b8000fb8ac118a8be013ed4b7daeda42a831
pramirez206 <pramirez206@unab.edu.co>
2021-04-21 09:28:10 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/example/DDDInventarios/DddInventariosApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.example.DDDInventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddInventariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/example/DDDInventarios/DddInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.example.DDDInventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
